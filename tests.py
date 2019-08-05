e75bd69ab790a2e2e304997336333d07113a9392
Mark Forrer <mark.forrer@lbl.gov>
2019-08-05 16:23:27 -0700

    Copied over test code also removed in EDD PR 539

--- tests.py
+++ tests.py
@@ -31,3 +31,34 @@ class HmacTests(TestCase):
             auth(request).headers['Authorization'],
             ':'.join(('1', self.KEY_ID, self.USER_ID, 'j7iHK4iYiELZlEtDWD8GJm04CWc='))
         )
+
+
+class IceTests(TestCase):
+    def test_entry_uri_pattern(self):
+        from rest.clients.ice.api import ICE_ENTRY_URL_PATTERN
+
+        # test matching against ICE URI's with a numeric ID
+        uri = "https://registry-test.jbei.org/entry/49194/"
+        match = ICE_ENTRY_URL_PATTERN.match(uri)
+        self.assertEqual("https", match.group(1))
+        self.assertEqual("registry-test.jbei.org", match.group(2))
+        self.assertEqual("49194", match.group(3))
+
+        # test matching against ICE URI's with a UUID
+        uri = (
+            "https://registry-test.jbei.org/entry/761ec36a-cd17-41b8-a348-45d7552d4f4f"
+        )
+        match = ICE_ENTRY_URL_PATTERN.match(uri)
+        self.assertEqual("https", match.group(1))
+        self.assertEqual("registry-test.jbei.org", match.group(2))
+        self.assertEqual("761ec36a-cd17-41b8-a348-45d7552d4f4f", match.group(3))
+
+        # verify non-match against invalid URLs
+        uri = "ftp://registry.jbei.org/entry/12345"
+        self.assertIsNone(ICE_ENTRY_URL_PATTERN.match(uri))
+        uri = "http://registry.jbei.org/admin/12345"
+        self.assertIsNone(ICE_ENTRY_URL_PATTERN.match(uri))
+        uri = "http://registry.jbei.org/entry/12345/experiments"
+        self.assertIsNone(ICE_ENTRY_URL_PATTERN.match(uri))
+        uri = "http://registry.jbei.org/entry/foobar"
+        self.assertIsNone(ICE_ENTRY_URL_PATTERN.match(uri))
